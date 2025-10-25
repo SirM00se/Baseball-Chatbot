197cd8f055e09f1d3048697982549c3c04adc32f
SirM00se <sirm00se15@gmail.com>
2025-10-24 23:42:40 -0500

    Changed vector database into sql format

---
+++ database.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sqlite3
+
+df = pd.read_csv("vector_metadata.csv")
+
+conn = sqlite3.connect("baseball_vectors.db")
+
+df.to_sql("rules", conn, if_exists="replace", index=False)
+
+conn.close()
+print("CSV saved as SQLite database!")
